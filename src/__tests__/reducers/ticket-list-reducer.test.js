c25dd86cce5af1fba2a6de58e5816b98720f6615
taylulz <taylulzcode@gmail.com>
2021-02-15 09:55:03 -0800

    add reducer and first test

---
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -0,0 +1,30 @@
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+describe('ticketListReducer', () => {
+
+  let action;
+  const ticketData = {
+    names: 'Taylor and Tina',
+    location: '5y',
+    issue: 'Redux is broken',
+    id: 1
+  };
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});  // first arg is current state, second is action applied to state
+
+    // test('Should successfully add new ticket data to masterTicketList', () => {
+    //   const { names, location, issue, id } = ticketData;
+    //   action = {
+    //     type: 'ADD_TICKET',
+    //   };
+    // });
+  });
+});
+    
+    
+    
+    
+    
+    
+  
