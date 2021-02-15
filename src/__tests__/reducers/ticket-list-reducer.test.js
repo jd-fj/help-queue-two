6b92b7e69953d2b20c93b66e186bef2618cf2340
taylulz <taylulzcode@gmail.com>
2021-02-15 11:08:00 -0800

    make sure everything is uptodate

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,32 +1,33 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
+
   let action;
+
   const currentState = {
-    1: {names: 'Ryan & Gosling',
+    1: {names: 'Ryan & Aimen',
     location: '4b',
-    issue: 'Redux action is not working correctly',
-    id: 1},
-    2: {names: 'Laura Croft & Me ',
-    location: '5d',
+    issue: 'Redux action is not working correctly.',
+    id: 1 },
+    2: {names: 'Jasmine and Justine',
+    location: '2a',
     issue: 'Reducer has side effects.',
-    id: 2}
+    id: 2 }
   }
 
-  // let action;
-  // const ticketData = {
-  //   names: 'Taylor and Tina',
-  //   location: '5y',
-  //   issue: 'Redux is broken',
-  //   id: 1
-  // };
+  const ticketData = {
+    names: 'Ryan & Aimen',
+    location: '4b',
+    issue: 'Redux action is not working correctly.',
+    id: 1
+  };
 
-  test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(ticketListReducer({}, {type: null})).toEqual({});
+  test('Should return default state if no action type is recognized', () => {
+    expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
   test('Should successfully add new ticket data to masterTicketList', () => {
-    const { names, location, issue, id } = currentState;
+    const { names, location, issue, id } = ticketData;
     action = {
       type: 'ADD_TICKET',
       names: names,
@@ -34,7 +35,6 @@ describe('ticketListReducer', () => {
       issue: issue,
       id: id
     };
-
     expect(ticketListReducer({}, action)).toEqual({
       [id] : {
         names: names,
@@ -46,17 +46,16 @@ describe('ticketListReducer', () => {
   });
 
   test('Should successfully delete a ticket', () => {
-    action = { 
+    action = {
       type: 'DELETE_TICKET',
       id: 1
     };
     expect(ticketListReducer(currentState, action)).toEqual({
-      2: {names: 'Laura Croft & Me ',
-      location: '5d',
-      issue: 'Reducer has side effects.',
-      id: 2}
+      2: {names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2 }
     });
   });
+
 });
-
-
